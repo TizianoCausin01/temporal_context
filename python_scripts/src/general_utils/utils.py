9ffeaccf889c787699141f7faa5449617a8a2c0b
TizianoCausin01 <causintiziano@gmail.com>
2026-01-04 19:12:04 +0100

    Fixed function to compare off-diagonal triu permutations

--- python_scripts/src/general_utils/utils.py
+++ python_scripts/src/general_utils/utils.py
@@ -835,7 +835,7 @@ def get_module_by_path(obj, attribute_path: str):
 
 
 """
-get_tril_perms
+get_triu_perms
 Generate all unique unordered pairs (i, j) from a list, with i preceding j.
 This corresponds to the lower-triangular (or upper-triangular) combinations
 without self-pairs or duplicates.
@@ -844,12 +844,12 @@ INPUT:
 OUTPUT:
     - tuples_list: list[tuple] -> List of pairs (i, j) with i != j and each pair appearing once.
 """
-def get_tril_perms(in_list: list):
+def get_triu_perms(in_list: list):
     tuples_list = []
     for idx, i in enumerate(in_list):
-        for j in in_list[idx+1:]:
+        for j in in_list[:idx]:
             tuples_list.append((i,j))
-        # end for j in in_list[idx+1:]:
+        # end for j in in_list[:idx]:
     # end for idx, i in enumerate(in_list):
     return tuples_list
 # EOF
