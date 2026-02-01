063c70797f6307589051797ef4bf93fec9aadcf4
TizianoCausin01 <causintiziano@gmail.com>
2026-02-01 11:12:00 -0500

    Modified script to extract features from ANNs

--- bash_scripts/img_feats_extraction_pooling.sh
+++ bash_scripts/img_feats_extraction_pooling.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=30:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=5 # number of processes
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=250G
 #SBATCH --account=livingstone       # account name
-#SBATCH --partition=priority # partition name
+#SBATCH --partition=short # partition name
 #SBATCH --job-name=img_feats_extraction
 #SBATCH --output=/home/tic569/output_jobs/%x.%j.out   # file name will be *job_name*.*job_id*
 cd /home/tic569/temporal_context/python_scripts/scripts
@@ -14,8 +14,8 @@ module load gcc/14.2.0
 module load python/3.13.1
 module load openmpi
 source ~/virtual_envs/temporal_context/bin/activate
-mpiexec python3 run_img_feats_extraction_pooling.py --model_name=$1 --num_components=$2 --batch_size=$3 --img_size=$4 --monkey_name=$5 --date=$6 --folder_name=$7 --pkg=$8 --pooling=$9
+mpiexec python3 run_img_feats_extraction_pooling.py --model_name=$1 --batch_size=$2 --img_size=$3 --folder_name=$4 --pkg=$5 --pooling=$6
 # example call 
-# sbatch img_feats_extraction.sh vit_l_16 1000 1100 384 paul 230204 fewer_occlusion timm 
+# sbatch img_feats_extraction_pooling.sh vit_l_16 1100 384 fewer_occlusion timm mean
 
 
